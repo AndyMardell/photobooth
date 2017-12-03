953b6dbdc5650f15ee3f5d676a273c4c8d8d4798
Andy Mardell <andy@mardell.me>
2017-12-03 12:34:25 +0000

    A working python script that uploads a file to dropbox

---
+++ dropbox-tmp.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import sys
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
+
+# Add OAuth2 access token here.
+# You can generate one for yourself in the App Console.
+# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
+TOKEN = 'access'
+
+LOCALFILE = 'localfilename.jpg'
+
+# Uploads contents of LOCALFILE to Dropbox
+def backup():
+    with open(LOCALFILE, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file
+        # are changed on upload
+        print("Uploading " + LOCALFILE + " to Dropbox as /" + LOCALFILE + "...")
+        try:
+            dbx.files_upload(f.read(), "/" + LOCALFILE, mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().reason.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+
+if __name__ == '__main__':
+    # Check for an access token
+    if (len(TOKEN) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token. "
+            "Open up backup-and-restore-example.py in a text editor and "
+            "paste in your token in line 14.")
+
+    # Create an instance of a Dropbox class, which can make requests to the API.
+    print("Creating a Dropbox object...")
+    dbx = dropbox.Dropbox(TOKEN)
+
+    # Check that the access token is valid
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        sys.exit("ERROR: Invalid access token; try re-generating an "
+            "access token from the app console on the web.")
+
+    # Create a backup of the current settings file
+    backup()
+
+    print("Done!")
